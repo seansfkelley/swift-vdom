290b7444be56a8a5cb675d5c265cecb2a2472c32
Sean Kelley <seansfkelley@gmail.com>
2017-08-12 17:20:06 -0700

    Initial Commit

---
+++ swift-vdom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-vdom
+//
+//  Created by Sean Kelley on 8/12/17.
+//  Copyright © 2017 Sean Kelley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
